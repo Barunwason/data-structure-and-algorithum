7647bea9da074ab683f5c1e23c7c292b8921f24e
Barunwason <128703765+Barunwason@users.noreply.github.com>
2023-03-27 21:08:09 +0530

    Add files via upload

---
+++ c++/subarray.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a[]={8,7,3,4,9,1};
+    int s,sum;
+    cout<<"enter the number";
+    cin>>s;
+    for(int i=0;i<6;i++){
+        for(int j=i;j<6;j++){
+            /*cout<<"="<<sum;*/
+            if(sum==s){
+                cout<<i<<j-1<<endl;
+                for(int l=i;l<j;l++){
+                    cout<<a[l];
+                }
+            }
+            sum=0;
+            
+            for(int k=i;k<j+1;k++){
+                sum+=a[k];
+                /*cout<<a[k]<<",";*/
+            }
+        }
+    }
+}
